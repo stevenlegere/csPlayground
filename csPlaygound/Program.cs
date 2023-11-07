5f92df894e9a6f3f0059a1817ee155abd068b779
stevenlegere <stevenmarklegere@gmail.com>
2023-11-07 14:19:48 +0000

    literal and variable values

--- csPlaygound/Program.cs
+++ csPlaygound/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code!");
+
+Console.WriteLine("This is the first line.");
+Console.Write("This is the second line.");
+
+Console.WriteLine("This is the first line.");
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
+
+// Store and retrieve data using literal and variable values
+    // ('') creates a charecter literal
+    // ("") creates a string literal
+    // (true) creates a boolean literal
+    // (123) creates an integer literal
+
+// Make new line using \n
+Console.Write("\n");
+
+Console.WriteLine('A');
+Console.WriteLine("A");
+Console.WriteLine(true);
+Console.WriteLine(123);
+
