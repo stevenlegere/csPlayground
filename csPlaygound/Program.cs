9205b81be608d0cbf6834875a63d3d9969df54d9
stevenlegere <stevenmarklegere@gmail.com>
2024-02-05 19:28:55 +0000

    Working with calculating school grades

--- csPlaygound/Program.cs
+++ csPlaygound/Program.cs
@@ -321,9 +321,46 @@ Console.WriteLine($"The temperature is {celsius} Celsius.");
 
     // Recommended solution
 int fahrenheit2 = 94;
-decimal celsius2 = (fahrenheit - 32m) * (5m / 9m);
+decimal celsius2 = (fahrenheit2 - 32m) * (5m / 9m);
 Console.WriteLine("The temperature is " + celsius2 + " Celsius.");
-// The temperature is 34.
+// The temperature is 34.444444444444444444444444447 Celsius.
 
+
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+int sophia1 = 93;
+int sophia2 = 87;
+int sophia3 = 98;
+int sophia4 = 95;
+int sophia5 = 100;
+
+int nicolas1 = 80;
+int nicolas2 = 83;
+int nicolas3 = 82;
+int nicolas4 = 88;
+int nicolas5 = 85;
+
+int zahirah1 = 84;
+int zahirah2 = 96;
+int zahirah3 = 73;
+int zahirah4 = 85;
+int zahirah5 = 79;
+
+int jeong1 = 90;
+int jeong2 = 92;
+int jeong3 = 98;
+int jeong4 = 100;
+int jeong5 = 97;
+
+int sophiaSum = 0;
+int nicolasSum = 0;
+int zahirahSum = 0;
+int jeongSum = 0;
+
+Console.WriteLine("Sophia: " + sophiaSum);
+Console.WriteLine("Nicolas: " + nicolasSum);
+Console.WriteLine("Zahirah: " + zahirahSum);
+Console.WriteLine("Jeong: " + jeongSum);
 
 
