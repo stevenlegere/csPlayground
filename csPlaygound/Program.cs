9fcd4e38c92a589a6be0c85d59a499525abbf368
stevenlegere <stevenmarklegere@gmail.com>
2024-02-20 21:18:42 +0000

    Student GPA Calculator

--- csPlaygound/Program.cs
+++ csPlaygound/Program.cs
@@ -387,6 +387,22 @@ if (message12.Contains("fox"))
     Console.WriteLine("What does the fox say?");
 }
 
+// Student GPA Calculator
+string studentName = "Sophia Johnson";
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Computer Science I";
+string course5Name = "Psychology 101";
+
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
+
+// Exercise - Store the numeric grade values for each course
+
 ////////////////////////////////////////////////////////////////////////////////////////
 
 // Call methods or classes from CSharpFreeCodeCamp.cs
