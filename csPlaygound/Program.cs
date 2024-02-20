dd9b58b56847420f3b560e576816d70e675e4b20
stevenlegere <stevenmarklegere@gmail.com>
2024-02-20 21:38:11 +0000

    Working on student grade exercise

--- csPlaygound/Program.cs
+++ csPlaygound/Program.cs
@@ -401,6 +401,22 @@ int course3Credit = 4;
 int course4Credit = 4;
 int course5Credit = 3;
 
+int gradeA = 4;
+int gradeB = 3;
+
+int course1Grade = gradeA;
+int course2Grade = gradeB;
+int course3Grade = gradeB;
+int course4Grade = gradeB;
+int course5Grade = gradeA;
+
+Console.WriteLine($"{course1Name} {course1Grade} {course1Credit}");
+Console.WriteLine($"{course2Name} {course2Grade} {course2Credit}");
+Console.WriteLine($"{course3Name} {course3Grade} {course3Credit}");
+Console.WriteLine($"{course4Name} {course4Grade} {course4Credit}");
+Console.WriteLine($"{course5Name} {course5Grade} {course5Credit}");
+
+
 // Exercise - Store the numeric grade values for each course
 
 ////////////////////////////////////////////////////////////////////////////////////////
