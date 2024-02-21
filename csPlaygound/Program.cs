a615996eeb59bf2eb2901c33ba6270f0ec86c749
stevenlegere <stevenmarklegere@gmail.com>
2024-02-21 21:50:21 +0000

    Working on GPA calculator

--- csPlaygound/Program.cs
+++ csPlaygound/Program.cs
@@ -425,6 +425,11 @@ totalGradePoints += course3Credit * course3Grade;
 totalGradePoints += course4Credit * course4Grade;
 totalGradePoints += course5Credit * course5Grade;
 
+decimal gradePointAverage = (decimal) totalGradePoints / totalCreditHours;
+
+int leadingDigit = (int) gradePointAverage;
+int firstDigit = (int) (gradePointAverage * 10) % 10;
+int secondDigit = (int) (gradePointAverage * 100) % 10;
 
 Console.WriteLine($"{course1Name} {course1Grade} {course1Credit}");
 Console.WriteLine($"{course2Name} {course2Grade} {course2Credit}");
@@ -432,6 +437,7 @@ Console.WriteLine($"{course3Name} {course3Grade} {course3Credit}");
 Console.WriteLine($"{course4Name} {course4Grade} {course4Credit}");
 Console.WriteLine($"{course5Name} {course5Grade} {course5Credit}");
 
+Console.WriteLine($"Final GPA: {leadingDigit}.{firstDigit}{secondDigit}");
 
 // Exercise - Store the numeric grade values for each course
 
